4155ad27a558406c12431a1b6f56d8c43d75e241
pc-ite <informaciones.ite@gmail.com>
2025-05-02 04:22:23 -0400

    actualizado con link con icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>ITE Fórmulas - Tu recurso educativo</title>
     <meta charset="UTF-8">
+    <link rel="icon" href="{{ asset('image/ite.ico') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Fórmulas de física, química, matemáticas y más para estudiantes">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
