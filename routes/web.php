365608e1aaebbdec2bd4fbe79774893daeed5f0f
pc-ite <informaciones.ite@gmail.com>
2025-06-10 18:03:50 -0400

    midelware

--- routes/web.php
+++ routes/web.php
@@ -29,61 +29,65 @@ Route::get('/', function () {
 
 
 Auth::routes();
-Route::post('enviar', [MateriaController::class,'enviar'])->name('contacto.enviar');
-Route::post('enviar/DFDS', [MateriaController::class,'enviarS'])->name('newsletter.suscribir');
 
-Route::get('matematicas',[MatematicaController::class, 'index']); 
-//Route::get('prueba',[EjemploController::class, 'listarAjax']); 
+Route::middleware(['auth'])->group(function () {
 
-Route::get('materias', [MateriaController::class,'index'])->name('materias.index');
-Route::get('materias/create', [MateriaController::class,'create'])->name('materias.create');
-Route::post('materias', [MateriaController::class,'store'])->name('materias.store');
-Route::get('materias/show/{materia}', [MateriaController::class,'show'])->name('materias.show');
-Route::get('materias/{materia}/edit', [MateriaController::class,'edit'])->name('materias.edit');
-Route::put('materias/{materia}', [MateriaController::class,'update'])->name('materias.update');
-Route::delete('eliminar/materia/{materia}', [MateriaController::class,'destroy'])->name('materias.eliminar');
+    Route::post('enviar', [MateriaController::class,'enviar'])->name('contacto.enviar')->middleware('auth');
+    Route::post('enviar/DFDS', [MateriaController::class,'enviarS'])->name('newsletter.suscribir');
 
+    Route::get('matematicas',[MatematicaController::class, 'index']); 
+    //Route::get('prueba',[EjemploController::class, 'listarAjax']); 
 
-Route::get('temas/{materia}', [TemaController::class,'index'])->name('temas.index');
-Route::get('temas/crear/{materia}', [TemaController::class,'crear'])->name('temas.crear');
-Route::post('temas', [TemaController::class,'store'])->name('temas.store');
-Route::get('temas/show/{tema}', [TemaController::class,'show'])->name('temas.show');
-Route::get('temas/{tema}/edit', [TemaController::class,'edit'])->name('temas.edit');
-Route::put('temas/actualizar/{tema}', [TemaController::class,'update'])->name('temas.update');
-Route::delete('eliminar/tema/{tema}', [TemaController::class,'destroy'])->name('temas.eliminar');
-
-Route::get('formulas/{tema}', [FormulaController::class,'index'])->name('formulas.index');
-Route::get('formulas/create/{tema}', [FormulaController::class,'create'])->name('formulas.create');
-Route::post('formulas', [FormulaController::class,'store'])->name('formulas.store');
-Route::get('formulas/show/{formula}', [FormulaController::class,'show'])->name('formulas.show');
-Route::get('formulas/{formula}/edit', [FormulaController::class,'edit'])->name('formulas.edit');
-Route::put('formulas/{formula}', [FormulaController::class,'update'])->name('formulas.update');
-Route::delete('eliminar/formula/{formula}', [FormulaController::class,'destroy'])->name('formulas.eliminar');
-
-Route::get('variables/{formula}', [VariableController::class,'index'])->name('variables.index');
-Route::get('variables/create/{formula}', [VariableController::class,'create'])->name('variables.create');
-Route::get('variable/store', [VariableController::class,'guardarAjax'])->name('variables.store');
-Route::get('variables/show/{variable}', [VariableController::class,'show'])->name('variables.show');
-Route::get('editar/variable/{variable}', [VariableController::class,'edit'])->name('variables.edit');
-Route::get('variable/update', [VariableController::class,'update'])->name('variables.update');
-Route::delete('eliminar/variable/{variable}', [VariableController::class,'destroy'])->name('variables.eliminar');
-
-Route::get('ejemplos/{formula}', [EjemploController::class,'index'])->name('ejemplos.index');
-Route::get('ejemplos/view/{formula}', [EjemploController::class,'show'])->name('ejemplos.show');
-Route::get('get/ejemplo/{formula}', [EjemploController::class,'ejemplos'])->name('ejemplos.view');
-Route::get('ejemplos/create/{formula}', [EjemploController::class,'create'])->name('ejemplos.create');
-Route::get('ejemplo/store', [EjemploController::class,'guardarAjax'])->name('ejemplos.store');
-// Route::get('ejemplos/show/{ejemplo}', [EjemploController::class,'show'])->name('ejemplos.show');
-Route::get('editar/ejemplo/{ejemplo}', [EjemploController::class,'edit'])->name('ejemplos.edit');
-Route::get('ejemplo/update', [EjemploController::class,'update'])->name('ejemplos.update');
-Route::get('ejemplos/listar/otro', [EjemploController::class,'listarAjax'])->name('ejemplos.listar');
-Route::delete('eliminar/ejemplo/{ejemplo}', [EjemploController::class,'destroy'])->name('ejemplos.eliminar');
-
-Route::get('get/dimensiones', [DimensionController::class,'getDimension'])->name('dimensiones.get');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/mostrarqr/{tema}', [App\Http\Controllers\FormulaController::class, 'mostrarQr'])->name('mostrar.qr');
+    Route::get('materias', [MateriaController::class,'index'])->name('materias.index');
+    Route::get('materias/create', [MateriaController::class,'create'])->name('materias.create');
+    Route::post('materias', [MateriaController::class,'store'])->name('materias.store');
+    Route::get('materias/show/{materia}', [MateriaController::class,'show'])->name('materias.show');
+    Route::get('materias/{materia}/edit', [MateriaController::class,'edit'])->name('materias.edit');
+    Route::put('materias/{materia}', [MateriaController::class,'update'])->name('materias.update');
+    Route::delete('eliminar/materia/{materia}', [MateriaController::class,'destroy'])->name('materias.eliminar');
 
 
+    Route::get('temas/{materia}', [TemaController::class,'index'])->name('temas.index');
+    Route::get('temas/crear/{materia}', [TemaController::class,'crear'])->name('temas.crear');
+    Route::post('temas', [TemaController::class,'store'])->name('temas.store');
+    Route::get('temas/show/{tema}', [TemaController::class,'show'])->name('temas.show');
+    Route::get('temas/{tema}/edit', [TemaController::class,'edit'])->name('temas.edit');
+    Route::put('temas/actualizar/{tema}', [TemaController::class,'update'])->name('temas.update');
+    Route::delete('eliminar/tema/{tema}', [TemaController::class,'destroy'])->name('temas.eliminar');
 
-Route::get('/descargar-formulario/{materia_id}/{tema_id}', [FormulaController::class, 'descargarFormulario'])->name('formulario.descargar')->middleware('auth');
+    Route::get('formulas/{tema}', [FormulaController::class,'index'])->name('formulas.index');
+    Route::get('formulas/create/{tema}', [FormulaController::class,'create'])->name('formulas.create');
+    Route::post('formulas', [FormulaController::class,'store'])->name('formulas.store');
+    Route::get('formulas/show/{formula}', [FormulaController::class,'show'])->name('formulas.show');
+    Route::get('formulas/{formula}/edit', [FormulaController::class,'edit'])->name('formulas.edit');
+    Route::put('formulas/{formula}', [FormulaController::class,'update'])->name('formulas.update');
+    Route::delete('eliminar/formula/{formula}', [FormulaController::class,'destroy'])->name('formulas.eliminar');
+
+    Route::get('variables/{formula}', [VariableController::class,'index'])->name('variables.index');
+    Route::get('variables/create/{formula}', [VariableController::class,'create'])->name('variables.create');
+    Route::get('variable/store', [VariableController::class,'guardarAjax'])->name('variables.store');
+    Route::get('variables/show/{variable}', [VariableController::class,'show'])->name('variables.show');
+    Route::get('editar/variable/{variable}', [VariableController::class,'edit'])->name('variables.edit');
+    Route::get('variable/update', [VariableController::class,'update'])->name('variables.update');
+    Route::delete('eliminar/variable/{variable}', [VariableController::class,'destroy'])->name('variables.eliminar');
+
+    Route::get('ejemplos/{formula}', [EjemploController::class,'index'])->name('ejemplos.index');
+    Route::get('ejemplos/view/{formula}', [EjemploController::class,'show'])->name('ejemplos.show');
+    Route::get('get/ejemplo/{formula}', [EjemploController::class,'ejemplos'])->name('ejemplos.view');
+    Route::get('ejemplos/create/{formula}', [EjemploController::class,'create'])->name('ejemplos.create');
+    Route::get('ejemplo/store', [EjemploController::class,'guardarAjax'])->name('ejemplos.store');
+    // Route::get('ejemplos/show/{ejemplo}', [EjemploController::class,'show'])->name('ejemplos.show');
+    Route::get('editar/ejemplo/{ejemplo}', [EjemploController::class,'edit'])->name('ejemplos.edit');
+    Route::get('ejemplo/update', [EjemploController::class,'update'])->name('ejemplos.update');
+    Route::get('ejemplos/listar/otro', [EjemploController::class,'listarAjax'])->name('ejemplos.listar');
+    Route::delete('eliminar/ejemplo/{ejemplo}', [EjemploController::class,'destroy'])->name('ejemplos.eliminar');
+
+    Route::get('get/dimensiones', [DimensionController::class,'getDimension'])->name('dimensiones.get');
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/mostrarqr/{tema}', [App\Http\Controllers\FormulaController::class, 'mostrarQr'])->name('mostrar.qr');
+
+
+
+    Route::get('/descargar-formulario/{materia_id}/{tema_id}', [FormulaController::class, 'descargarFormulario'])->name('formulario.descargar')->middleware('auth');
+});
